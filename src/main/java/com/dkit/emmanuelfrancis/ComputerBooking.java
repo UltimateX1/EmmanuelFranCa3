054712f5135bb6c863dfe346d7a9e01951faa550
emmanuel francis <D00228281@student.dkit.ie>
2020-11-25 23:13:31 +0000

    Added necessary classes
    Added necessary classes to my project and commited them

--- src/main/java/com/dkit/emmanuelfrancis/ComputerBooking.java
+++ src/main/java/com/dkit/emmanuelfrancis/ComputerBooking.java
@@ -1,2 +1,84 @@
-package com.dkit.emmanuelfrancis;public class ComputerBooking {
+package com.dkit.emmanuelfrancis;
+
+public class ComputerBooking {
+    public String bookingId;
+    public String dateTime;
+    public String returnDateTime;
+    public String computerType;
+    public String compAssetTag;
+    public String studentId;
+
+    public ComputerBooking(String bookingId, String dateTime, String returnDateTime, String computerType, String compAssetTag, String studentId) {
+        this.bookingId = bookingId;
+        this.dateTime = dateTime;
+        this.returnDateTime = returnDateTime;
+        this.computerType = computerType;
+        this.compAssetTag = compAssetTag;
+        this.studentId = studentId;
+    }
+
+    //Getters
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getReturnDateTime() {
+        return returnDateTime;
+    }
+
+    public String getComputerType() {
+        return computerType;
+    }
+
+    public String getCompAssetTag() {
+        return compAssetTag;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    //Setters
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setReturnDateTime(String returnDateTime) {
+        this.returnDateTime = returnDateTime;
+    }
+
+    public void setComputerType(String computerType) {
+        this.computerType = computerType;
+    }
+
+    public void setCompAssetTag(String compAssetTag) {
+        this.compAssetTag = compAssetTag;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "ComputerBooking{" +
+                "bookingId='" + bookingId + '\'' +
+                ", dateTime='" + dateTime + '\'' +
+                ", returnDateTime='" + returnDateTime + '\'' +
+                ", computerType='" + computerType + '\'' +
+                ", compAssetTag='" + compAssetTag + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/dkit/emmanuelfrancis/Student.java
+++ src/main/java/com/dkit/emmanuelfrancis/Student.java
@@ -1,2 +1,70 @@
-package com.dkit.emmanuelfrancis;public class Student {
+package com.dkit.emmanuelfrancis;
+
+public class Student {
+    private String name;
+    private String ID;
+    private String email;
+    private String telephone;
+    private String computersOnLoan;
+
+    public Student(String name, String ID, String email, String telephone, String computersOnLoan) {
+        this.name = name;
+        this.ID = ID;
+        this.email = email;
+        this.telephone = telephone;
+        this.computersOnLoan = computersOnLoan;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getComputersOnLoan() {
+        return computersOnLoan;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setComputersOnLoan(String computersOnLoan) {
+        this.computersOnLoan = computersOnLoan;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", ID='" + ID + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", computersOnLoan='" + computersOnLoan + '\'' +
+                '}';
+    }
 }
